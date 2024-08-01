ca19f2e807e5f1ef355d9bf7bcbef5dc8cd0724d
lucianolsa <luciano.ata@gmail.com>
2024-08-01 14:18:34 -0300

    Aula inicial 2sem

---
+++ abstracacao_de_dado.py
@@ -0,0 +1,9 @@
+class Fila:
+    def __init__(self):
+        self.fila = []
+    fila = []
+    def entrar(self, nome):
+        self.fila.append(nome)
+        print('Entrou')
+
+Fila.entrar('Luciano')
